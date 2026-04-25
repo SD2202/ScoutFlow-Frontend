8bed57f7b35f6d404d6e5140c2a72765a2594132
Sparsh Dwivedi <sparsh0302@gmail.com>
2026-04-26 04:07:19 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://scoutflow-backend-zxq0.onrender.com/api/'; 
+const API_BASE_URL = 'https://scoutflow-backend-zxq0.onrender.com'; 
 
 const api = axios.create({
   baseURL: API_BASE_URL,
